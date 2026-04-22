0aed4cecd022f06cbecc7ee3579cda31dd5806f2
ASH <0xash9@gmail.com>
2026-04-22 19:51:31 +0530

    FIXED DRAWING FLICKER

--- Frontend/src/utils/element.js
+++ Frontend/src/utils/element.js
@@ -5,23 +5,28 @@ import { TOOLS } from "../constants";
 const generator = rough.generator();
 export const createElement = (id, x1, y1, x2, y2, type, options) => {
     const element = {id, x1, y1, x2, y2};
-    options["seed"] = id + 1;
+    const cleanOptions = {
+        seed : id+1,
+        strokeWidth : options.strokeWidth,
+        stroke : options.stroke,
+        fill : options.fill
+    }
     switch(type) {
         case TOOLS.LINE : {
-            element.drawable = generator.line(x1, y1, x2, y2, options);
+            element.drawable = generator.line(x1, y1, x2, y2, cleanOptions);
             return element;
         }
         case TOOLS.RECT : {
-            element.drawable = generator.rectangle(x1, y1, x2-x1, y2-y1, options);
+            element.drawable = generator.rectangle(x1, y1, x2-x1, y2-y1, cleanOptions);
             return element;
         }
         case TOOLS.CIRCLE : {
-            element.drawable = generator.ellipse((x1+x2)/2, (y1+y2)/2, Math.abs(x2-x1), Math.abs(y2-y1), options);
+            element.drawable = generator.ellipse((x1+x2)/2, (y1+y2)/2, Math.abs(x2-x1), Math.abs(y2-y1), cleanOptions);
             return element;
         }
         case TOOLS.ARROW : {
             const {headX1, headY1, headX2, headY2} = getArrowHeadCoordinates(x1, y1, x2, y2);
-            element.drawable = generator.linearPath([[x1, y1], [x2, y2], [headX1, headY1], [x2, y2], [headX2, headY2]], options);
+            element.drawable = generator.linearPath([[x1, y1], [x2, y2], [headX1, headY1], [x2, y2], [headX2, headY2]], cleanOptions);
             return element;
         }
     }

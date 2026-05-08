262c9f06cf26f47b8efdebefb5fc5a79b5e5b156
ASH <0xash9@gmail.com>
2026-05-08 06:11:04 +0530

    Improved CustomPicker

--- Frontend/src/Components/ColorPicker/CustomPicker.jsx
+++ Frontend/src/Components/ColorPicker/CustomPicker.jsx
@@ -2,6 +2,8 @@ import React, { useMemo } from "react";
 import { RgbaStringColorPicker } from "react-colorful";
 import { colord, extend } from "colord";
 import namesPlugin from "colord/plugins/names";
+import classes from "./CustomPicker.module.css";
+
 extend([namesPlugin]);
 
 const CustomPicker = ({ color, ...rest }) => {
@@ -9,7 +11,24 @@ const CustomPicker = ({ color, ...rest }) => {
     return color.startsWith("rgba") ? color : colord(color).toRgbString();
   }, [color]);
 
-  return <RgbaStringColorPicker color={rgbaString} {...rest} />;
+  return (
+    <div className={classes.pickerContainer}>
+      <RgbaStringColorPicker 
+        color={rgbaString} 
+        {...rest} 
+        className={classes.customColorful}
+      />
+      <div className={classes.colorValues}>
+        <div 
+          className={classes.swatch} 
+          style={{ backgroundColor: rgbaString }} 
+        />
+        <span className={classes.hexText}>
+          {colord(rgbaString).toHex().toUpperCase()}
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default CustomPicker;
